1ff7c9bd02dcafce38919b080d839a2f0e7d4614
idguk <adrian_black@idg.co.uk>
2015-04-03 22:07:02 +0100

    Update web.sh

--- web.sh
+++ web.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 if [ ! -f "/var/setup-script-run" ] ; then
-sudo apt-get update
-sudo apt-get install -qy wget git
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt-get update
+#sudo apt-get update
+#sudo apt-get install -qy wget git
+#sudo apt-get install software-properties-common
+#sudo apt-add-repository ppa:ansible/ansible
+#sudo apt-get update
 sudo apt-get install -qy ansible
 
 sudo wget -qO- https://get.docker.com/ | sh
