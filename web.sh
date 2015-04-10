416a9de20c0b68da285a495eb88da3bfade7128c
ablack-idguk <adrian_black@idg.co.uk>
2015-04-10 17:42:16 +0100

    Update web.sh

--- web.sh
+++ web.sh
@@ -18,6 +18,8 @@ fi
 SITE_PREFIX=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/site" -H "Metadata-Flavor: Google")
 #export SITE_PREFIX 
 
+sudo curl "http://metadata.google.internal/computeMetadata/v1/project/attributes/vault-pass" -H "Metadata-Flavor: Google" > /root/vault-pass.txt
+
 sudo mkdir -p /etc/ansible/playbooks/
 sudo mkdir -p /etc/ansible/facts.d/
 sudo rm -f /etc/ansible/facts.d/site.fact
@@ -29,4 +31,4 @@ cd /etc/ansible/playbooks
 rm -f web-server.yml
 wget https://raw.githubusercontent.com/idguk/playbooks/master/web-server.yml
 
-ansible-playbook web-server.yml -i 'localhost,' --connection=local
+ansible-playbook web-server.yml -i 'localhost,' --connection=local --vault-password-file=/root/vault-pass.txt
