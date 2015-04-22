5c0b33f289bb9efea8c7690cac6937ee27189804
ablack-idguk <adrian_black@idg.co.uk>
2015-04-22 15:36:44 +0100

    added pip upgrade and remove sudo

--- web.sh
+++ web.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 #script run as root
 if [ ! -f "/var/setup-script-run" ] ; then
-#sudo apt-get update
-#sudo apt-get install -qy wget git
-sudo apt-get install software-properties-common
-sudo apt-add-repository -y ppa:ansible/ansible
-sudo apt-get update
-sudo apt-get install -qy ansible
+#apt-get update
+#apt-get install -qy wget git
+apt-get install software-properties-common
+apt-add-repository -y ppa:ansible/ansible
+apt-get update
+apt-get install -qy ansible
 
 sudo wget -qO- https://get.docker.com/ | sh
 
@@ -15,18 +15,20 @@ wget https://bootstrap.pypa.io/get-pip.py && python get-pip.py
 touch /var/setup-script-run
 fi
 
+sudo -H pip install --upgrade pip
+
 SITE_PREFIX=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/attributes/site" -H "Metadata-Flavor: Google")
 #export SITE_PREFIX 
 
 rm -f ~/vault-pass.txt
 curl "http://metadata.google.internal/computeMetadata/v1/project/attributes/vault-pass" -H "Metadata-Flavor: Google" > /root/vault-pass.txt
 
-sudo mkdir -p /etc/ansible/playbooks/
-sudo mkdir -p /etc/ansible/facts.d/
-sudo rm -f /etc/ansible/facts.d/site.fact
+mkdir -p /etc/ansible/playbooks/
+mkdir -p /etc/ansible/facts.d/
+rm -f /etc/ansible/facts.d/site.fact
 #touch ~/site.fact
 printf "[info]\nprefix=$SITE_PREFIX\n" > /tmp/site.fact
-sudo mv /tmp/site.fact /etc/ansible/facts.d/site.fact
+mv /tmp/site.fact /etc/ansible/facts.d/site.fact
 
 cd /etc/ansible/playbooks
 rm -f *.yml
